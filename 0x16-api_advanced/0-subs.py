915837bc9cd5a36a7377dcba1ef5e909006d885d
Athaba2016 <sumiefb@gmail.com>
2024-06-06 10:37:28 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -16,6 +16,6 @@ def number_of_subscribers(subreddit):
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
-        return
+        return 0
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
